c20ed6e2b9be72bcfec37786e21336a0e93f000a
Bibek Chand <bibekchand843@gmail.com>
2026-01-26 09:24:04 +0545

    Fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+import { Route, Routes } from "react-router";
 import DotGrid from "./components/DotGrid/DotGrid.jsx";
 import PillNav from "./components/PillNav/PillNav.jsx";
-import PixelCard from "./components/PixelCard/PixelCard.jsx";
-import Shuffle from "./components/Shuffle/Shuffle.jsx";
 import logo from "./logo.svg";
-import TextType from "./components/TextType/TextType.jsx";
-import Button from "./components/Button/Button.jsx";
-import  Links from "./components/Links/Links.jsx";
+import About from "./pages/About/About.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
 	return (
@@ -44,53 +42,10 @@ function App() {
 					initialLoadAnimation={false}
 				/>
 			</div>
-			<div className="text-center p-4">
-				<Shuffle
-					text="KEC Lite 2082"
-					shuffleDirection="right"
-					duration={0.35}
-					animationMode="evenodd"
-					shuffleTimes={1}
-					ease="power3.out"
-					stagger={0.03}
-					threshold={0.1}
-					triggerOnce={true}
-					triggerOnHover
-					respectReducedMotion={true}
-					loop={false}
-					loopDelay={0}
-				/>
-			</div>
-			<div>
-        </div>
-        <Links url="https://maps.app.goo.gl/TSgtXmcomGa4yaXz8"/>
-			<div className="text-5xl text-center p-6 relative">
-				<TextType
-					text={["We've go events for:", "Electronics", "Computer", "Civil"]}
-					typingSpeed={75}
-					pauseDuration={1500}
-					showCursor
-					cursorCharacter="_"
-					texts={[
-						"Welcome to React Bits! Good to see you!",
-						"Build some amazing experiences!",
-					]}
-					deletingSpeed={50}
-					variableSpeedEnabled={false}
-					variableSpeedMin={60}
-					variableSpeedMax={120}
-					cursorBlinkDuration={0.5}
-				/>
-			</div>
-
-			<div className="flex justify-center">
-				<PixelCard variant="pink">
-					<div className="absolute text-center ">
-						<div>Our Event</div>
-						<div>Our Event</div>
-					</div>
-				</PixelCard>
-			</div>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="about" element={<About />} />
+			</Routes>
 		</>
 	);
 }
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+export default function About(){
+    return (
+        <>
+        <div className="relative">
+            Hello
+            Hello
+        </div>
+        </>
+    )
+}
+
